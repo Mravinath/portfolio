3f17ae2d63fae70eb9f4db5e484033956bcfd21f
Mravinath <nravi7534@gmail.com>
2023-12-17 22:50:16 -0600

    new portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+     strings:["Frontend Developer","Student od CS", "Editer", "Coder"],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop: true
+
+    })
